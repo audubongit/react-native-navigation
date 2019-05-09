b60bac6664139ae091158fa5f656ca329b673354
Ivan Varga <ivan.varga.zg@gmail.com>
2019-05-09 11:28:52 +0200

    prevent-unwanted-app-destroy

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -55,6 +55,7 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
      * Along with that, we should handle commands from the bridge using onNewIntent
      */
     static NavigationActivity currentActivity;
+    static int numberOfActivities = 0;
     private static Promise startAppPromise;
 
     private ActivityParams activityParams;
@@ -73,7 +74,7 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
             finish();
             return;
         }
-
+        numberOfActivities += 1;
         activityParams = NavigationCommandsHandler.parseActivityParams(getIntent());
         disableActivityShowAnimationIfNeeded();
         setOrientation();
@@ -196,7 +197,7 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
     }
 
     private void destroyJsIfNeeded() {
-        if (currentActivity == null || currentActivity.isFinishing()) {
+        if ((currentActivity == null && numberOfActivities < 2) || (currentActivity != null && currentActivity.isFinishing())) {
             getReactGateway().onDestroyApp(this);
         }
     }
