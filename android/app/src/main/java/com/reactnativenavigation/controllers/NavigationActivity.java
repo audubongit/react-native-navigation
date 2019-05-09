0295b64eafe682decb6d83e50e207b910663093c
Ivan Varga <ivan.varga.zg@gmail.com>
2019-05-09 11:38:26 +0200

    fix when app is stuck on the splash screen

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -64,12 +64,15 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
     @Nullable
     private PermissionListener mPermissionListener;
 
+    boolean killedBySystem = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (!NavigationApplication.instance.getReactGateway().hasStartedCreatingContext() ||
                 getIntent() == null ||
                 getIntent().getBundleExtra("ACTIVITY_PARAMS_BUNDLE") == null) {
+            killedBySystem = true;
             SplashActivity.start(this);
             finish();
             return;
@@ -197,6 +200,10 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
     }
 
     private void destroyJsIfNeeded() {
+        if ( killedBySystem ) {
+            return;
+        }
+        
         if ((currentActivity == null && numberOfActivities < 2) || (currentActivity != null && currentActivity.isFinishing())) {
             getReactGateway().onDestroyApp(this);
         }
